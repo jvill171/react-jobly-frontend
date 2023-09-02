e8863e3246db2e73cb2525bb94dbff63b554ed86
jvill171 <juanvillarreal64@yahoo.com>
2023-09-02 16:19:48 -0500

    Created basic JobCard component

---
+++ src/components/JobCard/JobCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { commaWholeNum } from "../../helpers/helpers";
+import "./JobCard.css"
+
+const JobCard = ({data}) =>{
+    const {title, companyName, salary, equity, id}= data
+
+    const handleApply = () =>{
+        console.log("APPLIED FOR JOB WITH ID", id)
+    }
+
+    return(
+        <div className="JobCard">
+            <p><b>{title}</b></p>
+            {companyName ?
+                <p>{companyName}</p>
+                : "" 
+            }
+            <div className="job-details">
+                <p><b>Salary</b>: {commaWholeNum(salary) || "N/A"}</p>
+                <p><b>Equity</b>: {equity || "N/A"}</p>
+            </div>
+            <p><b>ID</b>: {id}</p>
+            <div className="job-apply">
+                <button onClick={handleApply}>APPLY</button>
+            </div>
+        </div>
+    )
+}
+export default JobCard;

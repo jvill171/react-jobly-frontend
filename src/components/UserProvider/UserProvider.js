a58c5cb5dcfdab02af288a1b1606d7449ebcb65f
jvill171 <juanvillarreal64@yahoo.com>
2023-09-07 15:12:32 -0500

    Created context for a username/user to be accessible through all routes

---
+++ src/components/UserProvider/UserProvider.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import NavBar from "../NavBar/NavBar";
+import RouteList from "../RouteList/RouteList";
+import JobApplyProvider from "../JobApplyProvider/JobApplyProvider";
+
+const UserProvider = () =>{
+
+    const [user, setUser] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(true)
+
+    const updateUser = () => {
+      try{
+        const token = localStorage.getItem(`_token`)
+        const { username } = jwtDecode(token)
+        setUser(username)
+      }catch(err){
+        // Authentication to routes that require
+        // authentication will be handled by backend
+        setUser() // Bad/no token = Invalid user; Log out
+        navigate("/") //After logout, redirect to "/"
+      }
+    };
+    
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+      if(!user){
+        updateUser()
+      }
+      setIsLoading(false)
+    },[user])
+    
+    return(
+        <UserContext.Provider value={{ user, updateUser }}>
+          <NavBar />
+        {isLoading ? <h1>LOADING...</h1> :
+          <JobApplyProvider
+            children={ <RouteList /> }
+          />
+        }
+        </UserContext.Provider>
+    )
+}
+
+export default UserProvider;

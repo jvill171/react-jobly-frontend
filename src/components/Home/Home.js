fbb1b19ca35a1d335eee585f60a1069f92d8bc9e
jvill171 <juanvillarreal64@yahoo.com>
2023-09-13 12:31:33 -0500

    Fixed typo where showed placeholder text instead of actual user

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,20 +5,19 @@ import { Link } from "react-router-dom";
 
 const Home = () =>{
     const {user} = useContext(UserContext)
+
     return(
         <div className="Home">
             <h1>Jobly</h1>
             <p>All the jobs in one, convenient place.</p>
             {user ?
-                <h2>Welcome Back, Exa!</h2>
+                <h2>Welcome Back, {user}</h2>
                 :
                 <div>
                     <Link to="/login"><button>Log in</button></Link>
                     <Link to="/signup"><button>Sign up</button></Link>
                 </div>
-
             }
-
         </div>
     )
 }
